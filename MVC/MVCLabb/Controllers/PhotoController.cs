efed74dda94d5ffac572b0af305068c497dfdf9b
CashmereBob <weidmar@gmail.com>
2016-11-15 16:06:49 +0100

    Photo CRUD påbörjat, update kvar.

--- MVC/MVCLabb/Controllers/PhotoController.cs
+++ MVC/MVCLabb/Controllers/PhotoController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MVCLabb.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,24 @@ namespace MVCLabb.Controllers
 {
     public class PhotoController : Controller
     {
+        private static IList<PhotoModel> photoes = new List<PhotoModel>();
         // GET: Photo
         public ActionResult Index()
         {
-            return View();
+            if (photoes.Count() < 1)
+            {
+                var photoDir = Directory.GetFiles(Server.MapPath("~/Photos"));
+                
+                foreach (var photo in photoDir)
+                {
+                    var info = new FileInfo(photo);
+
+                    photoes.Add(new PhotoModel { name = info.Name, id = Guid.NewGuid(), path = $" /Photos/{info.Name}" });
+                }
+            }
+
+
+            return View(photoes);
         }
 
         // GET: Photo/Details/5
@@ -28,17 +44,21 @@ namespace MVCLabb.Controllers
 
         // POST: Photo/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(PhotoModel photo, HttpPostedFileBase photoUpload)
         {
             try
             {
                 // TODO: Add insert logic here
 
+                photoUpload.SaveAs(Path.Combine(Server.MapPath("~/Photos"), photoUpload.FileName));
+
+                photoes.Add(new PhotoModel { name = photo.name, id = Guid.NewGuid(), path = $" /Photos/{photoUpload.FileName}"});
+
                 return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View(photo);
             }
         }
 
@@ -65,18 +85,26 @@ namespace MVCLabb.Controllers
         }
 
         // GET: Photo/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(PhotoModel photo)
         {
-            return View();
+            
+            return View(photoes.Where(x => x.id == photo.id).FirstOrDefault());
         }
 
         // POST: Photo/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(PhotoModel photo, FormCollection collection)
         {
             try
             {
                 // TODO: Add delete logic here
+
+                string fullPath = Request.MapPath(photoes.Where(x => x.id == photo.id).FirstOrDefault().path);
+                if (System.IO.File.Exists(fullPath))
+                {
+                    System.IO.File.Delete(fullPath);
+                    photoes.Remove(photoes.Where(x => x.id == photo.id).FirstOrDefault());
+                }
 
                 return RedirectToAction("Index");
             }
