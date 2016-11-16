9bbbe1e2dad61799554f232c307c0cb510559afd
CashmereBob <weidmar@gmail.com>
2016-11-16 09:24:14 +0100

    Masory effekt on galery index, flexbox and jquery

---
+++ MVC/MVCLabb/Scripts/MasonaryJavaScript.js
@@ -0,0 +1,25 @@
+﻿$(window).load(function () {
+    whileOverfloed();
+});
+
+$(document).resize(function () {
+    whileOverfloed();
+});
+
+function isOverflowed() {
+    var overflow = false;
+    $('#masonarycontainer').children('.masonaryitem').each(function () {
+        if ($('#masonarycontainer')[0].clientWidth < $(this).position().left) {
+            overflow = true;
+        }
+    });
+    return overflow;
+}
+function whileOverfloed() {
+
+    $('#masonarycontainer').height(500);
+    while (isOverflowed() === true) {
+        $('#masonarycontainer').height($('#masonarycontainer').height() + 50);
+    }
+
+}
